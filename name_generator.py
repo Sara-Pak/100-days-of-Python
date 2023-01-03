4a6271b1f8d4f21c18e8f7cf613fb7bf286fb9eb
Sara-pak <sarabpak@gmail.com>
2023-01-02 19:58:50 -0500

    initial commit - team name generator

---
+++ name_generator.py
@@ -0,0 +1,11 @@
+print('Welcome to the..')
+print('''
+╔╦╗┌─┐┌─┐┌┬┐  ╔╗╔┌─┐┌┬┐┌─┐  ╔═╗┌─┐┌┐┌┌─┐┬─┐┌─┐┌┬┐┌─┐┬─┐
+ ║ ├┤ ├─┤│││  ║║║├─┤│││├┤   ║ ╦├┤ │││├┤ ├┬┘├─┤ │ │ │├┬┘
+ ╩ └─┘┴ ┴┴ ┴  ╝╚╝┴ ┴┴ ┴└─┘  ╚═╝└─┘┘└┘└─┘┴└─┴ ┴ ┴ └─┘┴└─
+ ''')
+verb: str = input('What verb best describe your team? ')
+noun: str = input('What is one thing that everyone enjoy? ')
+
+team_name: str = 'Here is your new Team Name!'
+print(team_name + ' The ' + noun + ' ' + verb + 'ers! ')
