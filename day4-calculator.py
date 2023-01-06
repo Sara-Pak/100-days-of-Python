534fea510271b7f1b7adbc5c04dd57878c2a66ee
Sara-pak <sarabpak@gmail.com>
2023-01-06 08:35:34 -0500

    implemented ascii art, refactor logic to calculate how many cans of paint to cover a surface area ( h x w )

--- day4-calculator.py
+++ day4-calculator.py
@@ -20,7 +20,14 @@ _____________________
 
 import math
 
+
 def paint_calculation(height, width, cover):
     area = height * width
     num_of_cans_of_paint = math.ceil(area / cover)
     print(f"You'll need {num_of_cans_of_paint} cans of paint.")
+
+
+test_height = int(input("Height of wall: "))
+test_width = int(input("Width of wall: "))
+coverage = 5
+paint_calculation(height=test_height, width=test_width, cover=coverage)
